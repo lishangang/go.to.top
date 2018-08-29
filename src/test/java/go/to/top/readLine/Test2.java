0d9555db3f8b155de4c170c10a96a1a3fe444b60
lishangang <lishangang@9fbank.com.cn>
2018-08-29 18:25:02 +0800

    李四2次修改

--- src/test/java/go/to/top/readLine/Test2.java
+++ src/test/java/go/to/top/readLine/Test2.java
@@ -9,8 +9,8 @@ package go.to.top.readLine;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("嘿嘿，我又增加一行，你冲突了");
-		System.out.println("修改一下");
-		System.out.println("李四修改一下");
+		System.out.println("嘿嘿，我又增加苟富贵一行，你冲突了");
+		System.out.println("修改一颤三哈哈下");
+		System.out.println("李四修改放到颤三一下");
 	}
 }
