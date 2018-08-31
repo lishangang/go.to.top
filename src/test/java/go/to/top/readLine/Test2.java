75978a1c373a0fc0ecbfdfed667fc1bc49cad195
lishangang <lishangang@9fbank.com.cn>
2018-08-31 09:55:53 +0800

    李四修改了一下sys.prt,liangge

--- src/test/java/go/to/top/readLine/Test2.java
+++ src/test/java/go/to/top/readLine/Test2.java
@@ -9,8 +9,7 @@ package go.to.top.readLine;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("嘿嘿，我又增加苟富贵一行，你冲突了");
-		System.out.println("修改一颤三哈哈下");
-		System.out.println("李四修改放到颤三一下");
+		System.out.println("---------A--------");
+		System.out.println("---------B--------");
 	}
 }
