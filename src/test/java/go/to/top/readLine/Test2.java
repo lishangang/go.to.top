b34054320603bcc46f6b49194c734ac305bb7c9e
lishangang <lishangang@9fbank.com.cn>
2018-08-31 09:54:24 +0800

    张三改成一个system.print--------

--- src/test/java/go/to/top/readLine/Test2.java
+++ src/test/java/go/to/top/readLine/Test2.java
@@ -9,7 +9,6 @@ package go.to.top.readLine;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("嘿嘿，我又增加一行，你冲突了");
-		System.out.println("修改gffgfgfgffffffffffffff一下");
+		System.out.println("------------------------");
 	}
 }
